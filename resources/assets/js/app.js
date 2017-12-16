81ed9030c8d5a8b8347663f20583f64a7ad84487
Set Kyar Wa Lar <setkyar16@gmail.com>
2017-12-16 22:52:50 +0630

    add react part

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,21 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
-window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import Master from './components/Master';
+import CreateProduct from './components/CreateProduct';
+import DisplayProduct from './components/DisplayProduct';
+import UpdateProduct from './components/UpdateProduct';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+render(
+  <Router history={browserHistory}>
+      <Route path="/" component={Master} >
+        <Route path="/add-item" component={CreateProduct} />
+        <Route path="/display-item" component={DisplayProduct} />
+        <Route path="/edit/:id" component={UpdateProduct} />
+      </Route>
+    </Router>,
+        document.getElementById('crud-app'));
